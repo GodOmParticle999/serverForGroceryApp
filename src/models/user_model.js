be78dce3250f4895f669cbc36e14fdeb4977a8fa
NAVIN KUMAR SAHU <134603570+GodOmParticle999@users.noreply.github.com>
2025-03-09 21:33:43 +0530

    user_model implemented

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -1,23 +1,128 @@
 import mongoose from "mongoose";
 
 
+// user Schema
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         trim: true
     },
- 
-    address: {
-        type: String,
-        required: true
-    },
 
     role: {
         type: String,
-        enum:["Admin","Customer",]
+        enum:["Admin","Customer","DeliveryPartner"],
+        required: true,
     },
     isActivated:{
-
+           type:Boolean,
+           default:false
     }
 }, { timestamps: true });
+
+// Customer Schema
+
+const customerSchema = new mongoose.Schema({
+
+  ...userSchema.obj,
+   role:{
+    type:String,
+    enum:["Customer"],
+    default:"Customer"
+   },
+   phone:{
+    type:Number,
+    required:true,
+    unique:true,
+    trim:true,
+   }, 
+   liveLocation:{
+    longitude:{
+        type:Number,
+    },
+    latitude:{
+        type:Number,
+    }
+   },
+    address:{
+        type:String,
+    },
+    branch:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Branch"
+    }
+   
+}, { timestamps: true });
+
+
+// Delivery Partner Schema
+
+const deliveryPartnerSchema = new mongoose.Schema({
+    ...userSchema.obj,
+   role:{
+    type:String,
+    enum:["DeliveryPartner"],
+    default:"DeliveryPartner"
+   },
+   phone:{
+    type:Number,
+    required:true,
+    unique:true,
+    trim:true,
+   },
+   email:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+   },
+   password:{
+    type:String,
+    required:true,
+    trim:true,
+   },
+   liveLocation:{
+    longitude:{
+        type:Number,
+    },
+    latitude:{
+        type:Number,
+    }
+   },
+    address:{
+        type:String,
+    },
+    branch:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Branch"
+    }
+}, { timestamps: true });
+
+
+// Admin Schema
+
+const adminSchema = new mongoose.Schema({
+   ...userSchema.obj,
+   role:{
+    type:String,
+    enum:["Admin"],
+    default:"Admin"
+   },
+
+   email:{
+    type:String,
+    required:true,
+    unique:true,
+    trim:true,
+   },
+   password:{
+    type:String,
+    required:true,
+    trim:true,
+   },
+}, { timestamps: true });
+
+
+export const Customer = mongoose.model("Customer", customerSchema);
+export const DeliveryPartner = mongoose.model("DeliveryPartner", deliveryPartnerSchema);    
+export const Admin = mongoose.model("Admin", adminSchema);
