76541e5529df7d07442270d8e67ed84467a61a90
NAVIN KUMAR SAHU <134603570+GodOmParticle999@users.noreply.github.com>
2025-03-11 21:28:49 +0530

    auth controller >loginCustomer controller along with generate Tokens

--- src/controllers/auth_controller/auth_controller.js
+++ src/controllers/auth_controller/auth_controller.js
@@ -1,5 +1,35 @@
-import mongoose from "mongoose"
+import {Customer,DeliveryPartner} from "../../models/modelModule.js"
+import jwt from "jsonwebtoken"
 
-export const loginCustomer=async()=>{
+const generateTokens = async (user) => {
+    const accessToken = jwt.sign({id:user._id,role:user.role},process.env.ACCESS_TOKEN_SECRET ,{expiresIn:"1d"})
+    const refreshToken = jwt.sign({id:user._id,role:user.role},process.env.REFRESH_TOKEN_SECRET ,{expiresIn:"7d"})
+    return {accessToken,refreshToken}
+}
 
+// customer login
+
+export const customerLogin = async (req,reply) => {
+    try {
+      const {phone} = req.body
+      if(!phone){
+          return reply.status(400).json({message:"Please fill all the fields"})
+      }
+    let customer = await Customer.findOne({phone})
+    if(!customer){
+       customer=new Customer({
+         phone,
+         role:"Customer",
+         isActivated:true
+       })
+       await customer.save()
+    }
+    const {accessToken,refreshToken} = await generateTokens(customer)
+    return reply.status(200).json({message:"Login successful",accessToken,refreshToken,customer})
+  } catch (error) {
+   return reply.status(501).send({
+message:"An error occurred",error
+   })
+  }
+  
 }
